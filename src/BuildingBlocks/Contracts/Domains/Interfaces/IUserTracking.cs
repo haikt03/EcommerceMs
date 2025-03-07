1d49301e3badf8ba891b152ab6fae85ac5441432
haikt03 <tuanhai1652003@gmail.com>
2025-03-07 16:58:11 +0700

    Initial solution, adding logging, create base Product.API

---
+++ src/BuildingBlocks/Contracts/Domains/Interfaces/IUserTracking.cs
@@ -0,0 +1,6 @@
+﻿namespace Contracts.Domains.Interfaces
+{
+    internal interface IUserTracking
+    {
+    }
+}
---
+++ src/Services/Basket.API/Program.cs
@@ -0,0 +1,39 @@
+using Common.Logging;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog(Serilogger.Configure);
+
+Log.Information("Starting Basket API up");
+
+try
+{
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down Basket API complete");
+    Log.CloseAndFlush();
+}
