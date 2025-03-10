383964394527a7ee6d832eb0dba3a6aeb6c4987f
haikt03 <tuanhai1652003@gmail.com>
2025-03-10 17:50:00 +0700

    CRUD Customer.API

--- src/Services/Customer.API/Program.cs
+++ src/Services/Customer.API/Program.cs
@@ -1,4 +1,12 @@
 using Common.Logging;
+using Contracts.Common.Interfaces;
+using Customer.API.Persistence;
+using Customer.API.Repositories;
+using Customer.API.Repositories.Interfaces;
+using Customer.API.Services;
+using Customer.API.Services.Interfaces;
+using Infrastructure.Common;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +20,22 @@ try
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
+    builder.Services.AddDbContext<CustomerContext>(options => options.UseNpgsql(connectionString));
+
+    builder.Services.AddScoped<ICustomerRepository, CustomerRepository>()
+        .AddScoped(typeof(IRepositoryBaseAsync<,,>), typeof(RepositoryBaseAsync<,,>))
+        .AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>))
+        .AddScoped<ICustomerService, CustomerService>();
+
+
     var app = builder.Build();
+
+    app.MapGet("/api/customers/{username}", async (string username, ICustomerService customerService) =>
+    {
+        var customer = await customerService.GetCustomerByUserNameAsync(username);
+        return customer != null ? customer : Results.NotFound();
+    });
 
     if (app.Environment.IsDevelopment())
     {
@@ -26,11 +49,15 @@ try
 
     app.MapControllers();
 
-    app.Run();
+    app.SeedCustomerData().Run();
 }
 catch (Exception ex)
 {
-    Log.Fatal(ex, "Unhandled exception");
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+        throw;
+
+    Log.Fatal(ex, $"Unhandled exception: {ex.Message}");
 }
 finally
 {
